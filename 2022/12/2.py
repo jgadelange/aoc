e740a2f630c1814d60b25f1b9e5839aeb740de5d
Jeffrey de Lange <git@jeffreydelange.nl>
2022-12-12 07:24:00 +0100

    Day 12 2022

---
+++ 2022/12/2.py
@@ -0,0 +1,59 @@
+from queue import PriorityQueue
+
+f = open('./input', 'r')
+
+grid = [[x for x in line.strip()] for line in f.readlines()]
+w = len(grid[0])
+h = len(grid)
+
+start = None
+end = None
+
+queue = PriorityQueue()
+distance = {}
+
+for y in range(h):
+    for x in range(w):
+        if grid[y][x] == "S":
+            start = (y, x)
+            grid[y][x] = "a"
+        if grid[y][x] == "E":
+            end = (y, x)
+            grid[y][x] = "z"
+
+        if ord(grid[y][x]) == 97:
+            distance[(y,x)] = 0
+            queue.put((0, (y, x)))
+
+
+adj = [
+    (-1, 0),
+    (1, 0),
+    (0, 1),
+    (0, -1),
+]
+
+while queue.qsize():
+    d, (y, x) = queue.get()
+    curr = ord(grid[y][x])
+
+    for dy, dx in adj:
+        yy = y + dy
+        xx = x + dx
+        if (yy, xx) in distance:
+            continue
+        try:
+            a = ord(grid[yy][xx])
+        except IndexError:
+            continue
+
+        if a - curr <= 1:
+            distance[(yy, xx)] = d+1
+            queue.put((d+1, (yy, xx)))
+
+print(distance)
+print(distance[end])
+
+
+if __name__ == "__main__":
+    pass
