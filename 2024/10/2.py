d856fdc970268ee9e0ce4e5728c7f9556d83d7c2
Jeffrey de Lange <git@jeffreydelange.nl>
2024-12-10 07:44:48 +0100

    2024 day 10

---
+++ 2024/10/2.py
@@ -0,0 +1,54 @@
+import functools
+
+f = open('./input', 'r')
+
+grid = [
+    list(map(int, line.strip()))
+    for line in f.readlines()
+    if line.strip()
+]
+h, w = len(grid), len(grid[0])
+
+N = (0, -1)
+W = (-1, 0)
+S = (0, 1)
+E = (1, 0)
+
+ADJ_ORT = [N, E, S, W]
+
+
+def out_of_bounds(x, y):
+    return x < 0 or x >= w or y < 0 or y >= h
+
+
+starts = [
+    (x, y) for y in range(h) for x in range(w)
+    if grid[y][x] == 0
+]
+
+
+@functools.cache
+def trace(x, y):
+    c = grid[y][x]
+    if c == 9:
+        return 1
+
+    a = 0
+    for dx, dy in ADJ_ORT:
+        xx, yy = x + dx, y + dy
+        if out_of_bounds(xx, yy):
+            continue
+        if grid[yy][xx] == c+1:
+            a += trace(xx, yy)
+    return a
+
+ans = 0
+for x, y in starts:
+    a = trace(x, y)
+    ans += a
+    # print(len(a))
+# print(sum(trace(x, y) for x, y in starts))
+print(ans)
+
+if __name__ == "__main__":
+    pass
